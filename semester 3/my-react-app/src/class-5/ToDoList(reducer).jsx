4f0a465556926bb192c26c34acbfa44730a4df7f
Aaradhy Chinche <aaradhychinche@gmail.com>
2026-08-13 09:31:43 +0530

    semester 3: class 5 reducer todo

---
+++ semester 3/my-react-app/src/class-5/ToDoList(reducer).jsx
@@ -0,0 +1,47 @@
+import React, { useReducer, useState } from 'react'
+
+const ToDoList = () => {
+    
+     let data ={
+      input :"",
+      list:[]
+     }
+     let[state, dispatch]= useReducer(reduser, data)
+    function reduser(state,action){
+          if(action.type=="inp"){
+            return {
+              ...state,
+              input:action.payload
+
+            }
+          }else if (action.type=="add"){
+            return {
+              list:[...state.list , state.input]
+            }
+          }else if (action.type=="del"){
+              return{
+                ...state,
+                list : state.list.filter((_,id)=> id!==action.payload)
+              }
+          }
+    }
+  return (
+    <div>
+     <input onChange={(e)=>dispatch({type:"inp",payload:e.target.value})}></input>
+     <button onClick={()=>dispatch({type:"add"})}>add to list</button>
+     {
+  state.list.map((a,id)=>{
+      return(
+        <>
+        <li onClick={()=>dispatch({type:"del" , payload:id})}>{a}</li>
+
+        </>
+      )    
+      
+  })
+}
+    </div>
+  )
+}
+
+export default ToDoList
---
+++ semester 3/my-react-app/src/class-5/UseReducerDemo.jsx
@@ -0,0 +1,30 @@
+import React, { useReducer } from 'react'
+
+const UseReducerDemo = () => {
+    let[state,dispatch]=useReducer(reduser , 0)
+    function reduser(state,action){
+      if(action.type=="inc"){
+        return state+1
+      }else if (action.type=="dec"){
+        return state-1
+      }else if(action.type=="res"){
+        return 0
+      }else{
+        return state
+      }
+       
+
+
+    }
+  return (
+    <div>
+<h2>{state} </h2>
+<button onClick={()=>dispatch({type:"inc"})}>add</button>
+<button onClick={()=>dispatch({type:"dec"})}>dec</button>
+<button onClick={()=>dispatch({type:"res"})}>reset</button>
+
+    </div>
+  )
+}
+
+export default UseReducerDemo
